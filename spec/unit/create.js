1abbf77b53c6be94423f1e924a5fb0cab4ef9f9b
Barnaby Claydon <barnaby@swipely.com>
2013-02-18 23:13:18 -0500

    Further work on specs

---
+++ spec/unit/create.js
@@ -0,0 +1,93 @@
+// create.js
+var cradle = require('cradle');
+var YUI = require('yui').YUI
+
+YUI.add('specs:model-sync-couchdb:create', function (Y) {
+
+  describe('create', function () {
+
+    var subject,
+        Kitten = Y.Base.create('kitten',
+          Y.Model,
+          [Y.ModelSync.CouchDB],
+        {
+          databaseName: 'felines'
+        }),
+        KittenList = Y.Base.create('kittenList',
+          Y.ModelList,
+          [Y.ModelSync.CouchDB],
+        {
+
+          databaseName: 'felines',
+          model: Y.Kitten
+
+        });
+
+    describe('a single document', function () {
+      beforeEach(function () {
+
+        subject = new Kitten({
+          name  : 'Whiskers',
+          gender: 'male'
+          age   : 4
+        });
+
+        spyOn( subject, '_createDocument' ).andCallThrough();
+        spyOn( subject._db, 'save' );
+      });
+
+      it('creates a single document', function () {
+        subject.save();
+
+        expect( subject._createDocument ).toHaveBeenCalled();
+        expect( subject._db.save ).toHaveBeenCalledWith({
+          name  : 'Whiskers',
+          gender: 'male'
+          age   : 4
+        }, jasmine.any(Function));
+      });
+    });
+
+    describe('a list of documents', function () {
+      beforeEach(function () {
+
+        subject = new KittenList({
+          items: [
+            {
+              name  : 'Sparkles',
+              gender: 'female'
+              age   : 3
+            },
+            {
+              name  : 'Tinkles',
+              gender: 'female'
+              age   : 4
+            },
+            {
+              name  : 'Ernest',
+              gender: 'male'
+              age   : 5
+            },
+          ]
+        });
+
+        spyOn( subject, 'save' ).andCallThrough();
+
+      });
+
+      it('creates a list of documents', function () {
+
+      });
+    });
+
+  });
+
+},
+'0.0.1',
+{
+  requires: [
+    'model',
+    'model-list',
+    'model-sync-couchdb'
+  ]
+});
---
+++ spec/unit/read.js
@@ -0,0 +1,32 @@
+// read.js
+YUI.add('specs:model-sync-couchdb:read', function (Y) {
+
+  var cradle = require('cradle');
+
+  describe('read', function () {
+
+    var subject;
+
+    describe('a single document', function () {
+
+    });
+
+    describe('a list of documents', function () {
+
+    });
+
+    describe('a view', function () {
+
+    });
+
+  });
+
+},
+'0.0.1',
+{
+  requires: [
+    'model',
+    'model-list',
+    'model-sync-couchdb'
+  ]
+});
