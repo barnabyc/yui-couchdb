92209619f65d32d8c5b008f27d726940eada6179
Barnaby Claydon <barnaby@swipely.com>
2013-02-21 23:10:57 -0500

    Betterer Jasmine node module handling

--- spec/unit/create.js
+++ spec/unit/create.js
@@ -1,85 +1,91 @@
-// create.js
-var cradle = require('cradle');
-var Y = require('yui').use(
-  'model',
-  'model-list',
-  'model-sync-couchdb'
-);
+var YUI = require('yui').YUI;
+    // mod = require('yui-couchdb');
 
-describe('create', function () {
+YUI({
+  modules: {
+    'model-sync-couchdb': {
+        requires: [ 'model' ],
+        path: 'module.js'
+    }
+  }
+}).use( 'model-sync-couchdb', 'model', 'model-list', function(Y) {
 
-  var subject,
-      Kitten = Y.Base.create('kitten',
-        Y.Model,
-        [Y.ModelSync.CouchDB],
-      {
-        databaseName: 'felines'
-      }),
-      KittenList = Y.Base.create('kittenList',
-        Y.ModelList,
-        [Y.ModelSync.CouchDB],
-      {
+  describe('create', function () {
+    
+    var subject,
+        Kitten = Y.Base.create('kitten',
+          Y.Model,
+          [Y.ModelSync.CouchDB],
+        {
+          databaseName: 'felines'
+        }),
+        KittenList = Y.Base.create('kittenList',
+          Y.ModelList,
+          [Y.ModelSync.CouchDB],
+        {
 
-        databaseName: 'felines',
-        model: Y.Kitten
+          databaseName: 'felines',
+          model: Y.Kitten
+
+        });
+
+    describe('a single document', function () {
+      beforeEach(function () {
+
+        subject = new Kitten({
+          name  : 'Whiskers',
+          gender: 'male',
+          age   : 4
+        });
+
+        spyOn( subject, '_createDocument' ).andCallThrough();
+        spyOn( subject._db, 'save' );
+      });
+
+      it('creates a single document', function () {
+        subject.save();
+
+        expect( subject._createDocument ).toHaveBeenCalled();
+        expect( subject._db.save ).toHaveBeenCalledWith({
+          name  : 'Whiskers',
+          gender: 'male',
+          age   : 4
+        }, jasmine.any(Function));
+      });
+    });
+
+    describe('a list of documents', function () {
+      beforeEach(function () {
+
+        subject = new KittenList({
+          items: [
+            {
+              name  : 'Sparkles',
+              gender: 'female',
+              age   : 3
+            },
+            {
+              name  : 'Tinkles',
+              gender: 'female',
+              age   : 4
+            },
+            {
+              name  : 'Ernest',
+              gender: 'male',
+              age   : 5
+            },
+          ]
+        });
+
+        spyOn( subject, 'save' ).andCallThrough();
 
       });
 
-  describe('a single document', function () {
-    beforeEach(function () {
+      it('creates a list of documents', function () {
 
-      subject = new Kitten({
-        name  : 'Whiskers',
-        gender: 'male',
-        age   : 4
       });
-
-      spyOn( subject, '_createDocument' ).andCallThrough();
-      spyOn( subject._db, 'save' );
     });
 
-    it('creates a single document', function () {
-      subject.save();
-
-      expect( subject._createDocument ).toHaveBeenCalled();
-      expect( subject._db.save ).toHaveBeenCalledWith({
-        name  : 'Whiskers',
-        gender: 'male',
-        age   : 4
-      }, jasmine.any(Function));
-    });
-  });
-
-  describe('a list of documents', function () {
-    beforeEach(function () {
-
-      subject = new KittenList({
-        items: [
-          {
-            name  : 'Sparkles',
-            gender: 'female',
-            age   : 3
-          },
-          {
-            name  : 'Tinkles',
-            gender: 'female',
-            age   : 4
-          },
-          {
-            name  : 'Ernest',
-            gender: 'male',
-            age   : 5
-          },
-        ]
-      });
-
-      spyOn( subject, 'save' ).andCallThrough();
-
-    });
-
-    it('creates a list of documents', function () {
-
-    });
   });
 
 });
