3856b8d8b205006de2a58cdd796a36c1aa411a9f
Barnaby Claydon <barnaby@swipely.com>
2013-04-11 23:29:51 +0100

    Update list design doc view spec

--- spec/integration/list_spec.js
+++ spec/integration/list_spec.js
@@ -1,4 +1,7 @@
+var uuid = require('node-uuid');
+
 require('yui').getInstance().applyConfig({
+  // filter: 'debug',
   modules: {
     'model-sync-couchdb': require('../../index')
   }
@@ -10,6 +13,7 @@ describe('a list of documents', function () {
 
   var subject,
       callback,
+      databaseName = 'felines_'+uuid.v4(),
       Kitten = Y.Base.create('kitten',
         Y.Model,
         [],
@@ -23,7 +27,7 @@ describe('a list of documents', function () {
 
         model: Kitten,
 
-        databaseName: 'felines',
+        databaseName: databaseName,
 
         designDocuments: {
           breeds: {
@@ -48,6 +52,7 @@ describe('a list of documents', function () {
         ]
       });
 
+      spyOn( subject, '_createDesignDocuments' ).andCallThrough();
       spyOn( subject, '_createDocument' ).andCallThrough();
       spyOn( subject, 'parse' ).andCallThrough();
 
@@ -58,7 +63,12 @@ describe('a list of documents', function () {
       });
 
       waits(100);
+    });
 
+    it('calls `_createDesignDocuments`', function () {
+      runs(function () {
+        expect( subject._createDesignDocuments ).toHaveBeenCalled();
+      });
     });
 
     it('calls `_createDocument`', function () {
@@ -67,6 +77,8 @@ describe('a list of documents', function () {
       });
     });
 
+    // @todo this isn't going to be called as we're
+    // extending ModelList with `save`
     it('parses the response', function () {
       runs(function () {
         expect( subject.parse ).toHaveBeenCalledWith([
@@ -79,7 +91,42 @@ describe('a list of documents', function () {
   });
 
   describe('can be read', function () {
-    // @todo
+    beforeEach(function () {
+      spyOn( subject, '_queryAll' ).andCallThrough();
+      spyOn( subject, '_queryView' ).andCallThrough();
+      spyOn( subject, 'parse' ).andCallThrough();
+
+      callback = jasmine.createSpy();
+
+      runs(function () {
+        subject.load( callback );
+      });
+
+      waits(100);
+    });
+
+    it('calls `_queryAll', function () {
+      expect( subject._queryAll ).toHaveBeenCalledWith(
+      {
+        // no options
+      },
+      jasmine.any(Function));
+    });
+
+    it('calls `_queryView', function () {
+      expect( subject._queryView ).toHaveBeenCalledWith(
+      'all',
+      {
+        // no options
+      },
+      jasmine.any(Function));
+    });
+
+    it('parses the list of documents', function () {
+      expect( subject.parse ).toHaveBeenCalledWith({
+        pickles: 'yum'
+      });
+    });
   });
 
   describe('can be updated', function () {
